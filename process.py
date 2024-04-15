4fbd6882f426d509e67d162047702e2bd1c8ae4a
ys434 <ys434@cornell.edu>
2024-04-14 20:37:50 -0400

    sarima param

--- process.py
+++ process.py
@@ -9,11 +9,8 @@ for i in os.listdir(data_path):
     if i.endswith(".csv"):
         strings = i.split("_")
         year = int(strings[0])
-        # print(year)
         loc = strings[1][:-5]
-        # print(loc)
         num = strings[1][-5:-4]
-        # print(num)
         data = pd.read_csv("./data/"+i,skiprows=2).iloc[:,1:]
         colnames = data.columns.values
         for i in range(len(colnames)):
@@ -77,7 +74,6 @@ def sarima_helper(df, order, seasonal_order, steps):
     forecast = model_fit.forecast(steps=steps)
     return forecast
 
-
 def sarima(df, idx, order = (1,0,0), seasonal_order = (0,0,0,1), steps = 1):
     forecasts = {}
     for col in df.columns:
@@ -91,63 +87,83 @@ def sarima(df, idx, order = (1,0,0), seasonal_order = (0,0,0,1), steps = 1):
             ranking = i
     return ranking - 1
 
-p = [0,1,2]
-d = [0,1]
-q = [0,1,1]
-P = [0,1,2]
+p = [1]
+d = [0]
+q = [0]
+P = [0,1]
 D = [0,1]
-Q = [0,1,2]
+Q = [0,1]
 s = [2,4,12]
 
-# def sarima_param():
-#     best_us_para = []
-#     best_us_score = 100
-#     best_world_para = []
-#     best_world_score = 100
-#     for a in p:
-#         for b in d:
-#             for c in q:
-#                 for e in P:
-#                     for f in D:
-#                         for g in Q:
-#                             for h in s:
-#                                 para = [a,b,c,e,f,g,h]
-#                                 for i in range(len())
-
-
-for i in range(len(us_dataset)):
-    # print(str(2015+i)+"---Maximum------------------------------------------")
-    # print(str(2015+i)+"---US------------------------------------------")
-    # max_us = maximum(us_dataset[i], i)
-    # print(max_us)
-    # print(str(2015+i)+"---WORLD------------------------------------------")
-    # max_world = maximum(world_dataset[i], i)
-    # print(max_world)
-
-    # print(str(2015+i)+"---Mean------------------------------------------")
-    # print(str(2015+i)+"---US------------------------------------------")
-    # mean_us = mean(us_dataset[i], i)
-    # print(mean_us)
-    # print(str(2015+i)+"---WORLD------------------------------------------")
-    # mean_world = mean(world_dataset[i], i)
-    # print(mean_world)
-
-    # print(str(2015+i)+"---Exp Smoothing------------------------------------------")
-    # alpha = 0.5
-    # print(str(2015+i)+"---US------------------------------------------")
-    # exp_us = exponential_smoothing(us_dataset[i], alpha, i)
-    # print(exp_us)
-    # print(str(2015+i)+"---WORLD------------------------------------------")
-    # exp_wolrd = exponential_smoothing(world_dataset[i], alpha, i)
-    # print(exp_wolrd)
-
-    print(str(2015+i)+"---SARIMA------------------------------------------")
-    order = (1,1,1)
-    seasonal_order = (1,1,1,5)
+def sarima_param():
+    best_us_para = []
+    best_us_diff = 100
+    best_world_para = []
+    best_world_diff = 100
     steps = 1
-    print(str(2015+i)+"---US------------------------------------------")
-    sarima_us = sarima(us_dataset[i], i, order, seasonal_order, steps)
-    print(sarima_us)
-    print(str(2015+i)+"---WORLD------------------------------------------")
-    sarima_world = sarima(world_dataset[i], i, order, seasonal_order, steps)
-    print(sarima_world)
+    for a in p:
+        for b in d:
+            for c in q:
+                for e in P:
+                    for f in D:
+                        for g in Q:
+                            for h in s:
+                                para = [a,b,c,e,f,g,h]
+                                print(para)
+                                order = (a,b,c)
+                                seasonal_order = (e,f,g,h)
+                                diff_us = 0
+                                diff_world = 0
+                                for i in range(len(us_dataset)):
+                                    r_us = sarima(us_dataset[i], i, order, seasonal_order, steps)
+                                    diff_us += r_us
+                                    r_world = sarima(world_dataset[i], i, order, seasonal_order, steps)
+                                    diff_world += r_world
+                                if (diff_us < best_us_diff):
+                                    best_us_diff = diff_us
+                                    best_us_para = para
+                                if (diff_world < best_world_diff):
+                                    best_world_diff = diff_world
+                                    best_world_para = para
+    return best_us_para, best_us_diff, best_world_para, best_world_diff
+
+print(sarima_param())
+
+
+
+# for i in range(len(us_dataset)):
+#     print(str(2015+i)+"---Maximum------------------------------------------")
+#     print(str(2015+i)+"---US------------------------------------------")
+#     max_us = maximum(us_dataset[i], i)
+#     print(max_us)
+#     print(str(2015+i)+"---WORLD------------------------------------------")
+#     max_world = maximum(world_dataset[i], i)
+#     print(max_world)
+
+#     print(str(2015+i)+"---Mean------------------------------------------")
+#     print(str(2015+i)+"---US------------------------------------------")
+#     mean_us = mean(us_dataset[i], i)
+#     print(mean_us)
+#     print(str(2015+i)+"---WORLD------------------------------------------")
+#     mean_world = mean(world_dataset[i], i)
+#     print(mean_world)
+
+#     print(str(2015+i)+"---Exp Smoothing------------------------------------------")
+#     alpha = 0.5
+#     print(str(2015+i)+"---US------------------------------------------")
+#     exp_us = exponential_smoothing(us_dataset[i], alpha, i)
+#     print(exp_us)
+#     print(str(2015+i)+"---WORLD------------------------------------------")
+#     exp_wolrd = exponential_smoothing(world_dataset[i], alpha, i)
+#     print(exp_wolrd)
+
+#     print(str(2015+i)+"---SARIMA------------------------------------------")
+#     order = (1,1,1)
+#     seasonal_order = (1,1,1,5)
+#     steps = 1
+#     print(str(2015+i)+"---US------------------------------------------")
+#     sarima_us = sarima(us_dataset[i], i, order, seasonal_order, steps)
+#     print(sarima_us)
+#     print(str(2015+i)+"---WORLD------------------------------------------")
+#     sarima_world = sarima(world_dataset[i], i, order, seasonal_order, steps)
+#     print(sarima_world)
